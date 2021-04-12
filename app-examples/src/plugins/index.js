7528d7881a8d55d64f9dd2c3299f5c788a6f9d49
retrofox <rdsuarez@gmail.com>
2021-04-12 20:52:33 -0300

    plugin: add wcca-plugin-sidebar

---
+++ app-examples/src/plugins/index.js
@@ -0,0 +1,26 @@
+/**
+ * WordPress dependencies
+ */
+import { registerPlugin } from '@wordpress/plugins';
+import { PluginSidebar } from '@wordpress/edit-post';
+import { __ } from '@wordpress/i18n';
+
+import './style.scss'
+
+registerPlugin( 'wcca-plugin-sidebar', {
+	render: function() {
+		return (
+			<PluginSidebar
+				className="wcca-plugin-sidebar-panel"
+				name={ __( 'wcca-plugin-sidebar', 'app-example' ) }
+				icon={ 'palmtree' }
+				title={ __( 'WCCA @2021', 'app-example' ) }
+			>
+				<img
+					className="portada-hero"
+					src="http://localhost:8888/wp-content/uploads/2021/04/wcca-portada-hero.png"
+				/>
+			</PluginSidebar>
+		);
+	}
+} );
