0f5b148ee65c95d56149d9447ffd3b025bde0b86
retrofox <rdsuarez@gmail.com>
2021-04-12 18:16:45 -0300

    app structure

---
+++ app-examples/app-example.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Plugin Name:       Example App
+ * Description:       WordCamp America Central talk. https:&#x2F;&#x2F;centroamerica.wordcamp.org&#x2F;2021&#x2F;
+ * Requires at least: 5.7
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Damián Suárez (@retrofox)
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       app-example
+ *
+ * @package           wpac
+ */
+
+/**
+ * Registers the block using the metadata loaded from the `block.json` file.
+ * Behind the scenes, it registers also all assets so they can be enqueued
+ * through the block editor in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/writing-your-first-block-type/
+ */
+function wpac_app_example_block_init() {
+	register_block_type_from_metadata( __DIR__ );
+}
+add_action( 'init', 'wpac_app_example_block_init' );
