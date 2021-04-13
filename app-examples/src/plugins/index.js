cc358e8d290551e7e035f17f9e0aed901329aee6
retrofox <rdsuarez@gmail.com>
2021-04-13 11:12:55 -0300

    sidebar: add basic TextControl

--- app-examples/src/plugins/index.js
+++ app-examples/src/plugins/index.js
@@ -4,6 +4,8 @@
 import { registerPlugin } from '@wordpress/plugins';
 import { PluginSidebar } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
+import { Panel, PanelBody, TextControl } from '@wordpress/components';
+import { useState } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -12,6 +14,7 @@ import './style.scss'
 
 registerPlugin( 'wcca-plugin-sidebar', {
 	render: function() {
+		const [ value, setValue ] = useState( '' );
 		return (
 			<PluginSidebar
 				className="wcca-plugin-sidebar-panel"
@@ -23,6 +26,16 @@ registerPlugin( 'wcca-plugin-sidebar', {
 					className="portada-hero"
 					src="http://localhost:8888/wp-content/uploads/2021/04/portada-hero.png"
 				/>
+
+				<Panel>
+					<PanelBody title={ __( 'WCCA Tools', 'app-example' ) }>
+						<TextControl
+							label={ __( 'Search', 'app-example' ) }
+							value={ value }
+							onChange={ setValue }
+						/>
+					</PanelBody>
+				</Panel>
 			</PluginSidebar>
 		);
 	}
