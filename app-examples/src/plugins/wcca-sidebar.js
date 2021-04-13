13a4fffa1a50508c2bad7763ac0b478615b49ed2
retrofox <rdsuarez@gmail.com>
2021-04-13 16:14:11 -0300

    plugin: refactoring. tweak more button

---
+++ app-examples/src/plugins/wcca-sidebar.js
@@ -0,0 +1,52 @@
+/**
+ * WordPress dependencies
+ */
+import { registerPlugin } from '@wordpress/plugins';
+import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
+import { __ } from '@wordpress/i18n';
+import { Panel, PanelBody, TextControl } from '@wordpress/components';
+import { useState, Fragment } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss'
+
+registerPlugin( 'wcca-plugin-sidebar', {
+	render: function() {
+		const [ value, setValue ] = useState( '' );
+		return (
+			<Fragment>
+				<PluginSidebarMoreMenuItem
+					target="wcca-plugin-sidebar"
+					icon="palmtree"
+					title={ __( 'Open the WCCA plugin toolbar', 'app-example' ) }
+				>
+					{ __( 'WCCA', 'app-example' ) }
+				</PluginSidebarMoreMenuItem>
+				
+				<PluginSidebar
+					className="wcca-plugin-sidebar-panel"
+					name="wcca-plugin-sidebar"
+					icon="palmtree"
+					title={ __( 'WCCA @2021', 'app-example' ) }
+				>
+					<img
+						className="portada-hero"
+						src="http://localhost:8888/wp-content/uploads/2021/04/portada-hero.png"
+					/>
+
+					<Panel>
+						<PanelBody title={ __( 'WCCA Tools', 'app-example' ) }>
+							<TextControl
+								label={ __( 'Search', 'app-example' ) }
+								value={ value }
+								onChange={ setValue }
+							/>
+						</PanelBody>
+					</Panel>
+				</PluginSidebar>
+			</Fragment>
+		);
+	}
+} );
