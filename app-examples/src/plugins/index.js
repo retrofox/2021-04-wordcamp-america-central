b12b30d718f1f08973af14c1fae2573e1ed15f53
retrofox <rdsuarez@gmail.com>
2021-04-13 16:03:53 -0300

    sidebar: add more menu item

--- app-examples/src/plugins/index.js
+++ app-examples/src/plugins/index.js
@@ -2,10 +2,10 @@
  * WordPress dependencies
  */
 import { registerPlugin } from '@wordpress/plugins';
-import { PluginSidebar } from '@wordpress/edit-post';
+import { PluginSidebar, PluginSidebarMoreMenuItem } from '@wordpress/edit-post';
 import { __ } from '@wordpress/i18n';
 import { Panel, PanelBody, TextControl } from '@wordpress/components';
-import { useState } from '@wordpress/element';
+import { useState, Fragment } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -16,27 +16,33 @@ registerPlugin( 'wcca-plugin-sidebar', {
 	render: function() {
 		const [ value, setValue ] = useState( '' );
 		return (
-			<PluginSidebar
-				className="wcca-plugin-sidebar-panel"
-				name={ __( 'wcca-plugin-sidebar', 'app-example' ) }
-				icon={ 'palmtree' }
-				title={ __( 'WCCA @2021', 'app-example' ) }
-			>
-				<img
-					className="portada-hero"
-					src="http://localhost:8888/wp-content/uploads/2021/04/portada-hero.png"
-				/>
+			<Fragment>
+				<PluginSidebarMoreMenuItem target="wcca-plugin-sidebar" icon="palmtree">
+					Expanded Sidebar - More item
+				</PluginSidebarMoreMenuItem>
+				
+				<PluginSidebar
+					className="wcca-plugin-sidebar-panel"
+					name="wcca-plugin-sidebar"
+					icon="palmtree"
+					title={ __( 'WCCA @2021', 'app-example' ) }
+				>
+					<img
+						className="portada-hero"
+						src="http://localhost:8888/wp-content/uploads/2021/04/portada-hero.png"
+					/>
 
-				<Panel>
-					<PanelBody title={ __( 'WCCA Tools', 'app-example' ) }>
-						<TextControl
-							label={ __( 'Search', 'app-example' ) }
-							value={ value }
-							onChange={ setValue }
-						/>
-					</PanelBody>
-				</Panel>
-			</PluginSidebar>
+					<Panel>
+						<PanelBody title={ __( 'WCCA Tools', 'app-example' ) }>
+							<TextControl
+								label={ __( 'Search', 'app-example' ) }
+								value={ value }
+								onChange={ setValue }
+							/>
+						</PanelBody>
+					</Panel>
+				</PluginSidebar>
+			</Fragment>
 		);
 	}
 } );
